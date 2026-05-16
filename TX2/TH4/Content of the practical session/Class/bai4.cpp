962d84b2055b94838c09a3ea1534b95261193803
TuyenMinh <leminhtuyen280906@gmail.com>
2026-05-16 23:48:06 +0700

    fix: directory layout

---
+++ TX2/TH4/Content of the practical session/Class/bai4.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+//bai toan xep ba lo:
+//tu tuong là sắp xếp các gói hàng theo giá trị/trọng lượng giảm dần 
+// rồi lần lượt lấy từ trên xuống
+//mục đích là để lấy được các sản phẩm mà có cái giá trị / 1 đơn vị khối lượng theo chiều từ cao xuống thấp
+using namespace std;
