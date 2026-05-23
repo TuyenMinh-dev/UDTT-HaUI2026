7a2e933d0915b98658a2147dd9dd615c46d5bd53
TuyenMinh <leminhtuyen280906@gmail.com>
2026-05-23 15:35:15 +0700

    fix: clean code

--- TX2/train/private/de1/LeMinhTuyen_2024609044.cpp
+++ TX2/train/private/de1/LeMinhTuyen_2024609044.cpp
@@ -1,79 +1,122 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct Tu{
+struct Tu
+{
     string ten;
     int kg;
     int lit;
 };
-void A(int &k,int &n,vector<Tu> d){
-    int slg=0;
-    int klg=0;
-    vector<string> t;
-    for(int i=0;i<n;i++){
-        if(klg+d[i].kg<=k){
-            slg++;
-            klg+=d[i].kg;
-            t.push_back(d[i].ten);
+struct KetQuaA
+{
+    int v;
+    vector<Tu> t;
+};
+struct KetQuaB
+{
+    int p;
+    int q;
+    vector<Tu> ds;
+};
+KetQuaA A(int k, int n, vector<Tu> d)
+{
+    KetQuaA kq;
+    kq.v = 0; // giá trị sẽ trả về sau cùng
+    int klg = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (klg + d[i].kg <= k)
+        {
+            kq.v++;
+            klg += d[i].kg;
+            kq.t.push_back(d[i]);
+        }
+        else
+        {
+            break;
         }
     }
-    if(slg!=0){
-        cout<<"v= "<<slg<<endl;
-        cout<<"DANH SACH t gom: "<<endl;
-        for(string s : t){
-            cout<<s<<" ";
-        }cout<<endl;
-    }
-    else{
-        cout<<"Khong co phuong an"<<endl;
-    }
+    return kq; // trả về struct chứa v và t
 }
-void B(int &w,int &n,vector<Tu> d){
-    int dp[n+1][w+1];
-    memset(dp,0,sizeof(dp));
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=w;j++){
-            dp[i][j]=dp[i-1][j];
-            if(j >= d[i-1].lit){
-                dp[i][j]=max(dp[i][j],dp[i-1][j-d[i-1].lit]+d[i-1].kg);
+KetQuaB B(int w, int n, vector<Tu> d)
+{
+    int dp[n + 1][w + 1];
+    memset(dp, 0, sizeof(dp));
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= w; j++)
+        {
+            dp[i][j] = dp[i - 1][j];
+            if (j >= d[i - 1].lit)
+            {
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - d[i - 1].lit] + d[i - 1].kg);
             }
         }
     }
-    int slg=0;
-    int i=n;int j=w;
-    vector<string> a;
-    while(i>0 ){
-        if(dp[i][j]!=dp[i-1][j]){
-            slg++;
-            j-=d[i-1].lit;
-            a.push_back(d[i-1].ten);
+    int i = n;
+    int j = w;
+    KetQuaB kq;
+    kq.q = dp[n][w];
+    kq.p = 0;
+    while (i > 0 && j > 0)
+    {
+        if (dp[i][j] != dp[i - 1][j])
+        {
+            kq.p++;
+            j -= d[i - 1].lit;
+            kq.ds.push_back(d[i - 1]);
         }
         i--;
     }
-    if(slg>0){
-        cout<<"p= "<<slg<<endl;
-        cout<<"q= "<<dp[n][w]<<endl;
-        for(string x : a){
-            cout<<x<<" ";
-        }cout<<endl;
-    }
-    else{
-        cout<<"Khong co phuong an"<<endl;
+    reverse(kq.ds.begin(), kq.ds.end());
+    return kq;
+}
+void inDS(vector<Tu> ds)
+{
+    for (int i = 0; i < ds.size(); i++)
+    {
+        cout << " " << i + 1 << ". Ten: " << ds[i].ten
+             << " | Khoi luong: " << ds[i].kg << " kg"
+             << " | Dung tich: " << ds[i].lit << " lit\n";
     }
 }
-int main(){
-    int k=18;
-    int w=25;
-    int n=6;
+void inKQ(KetQuaA &kqA, KetQuaB &kqB)
+{
+    if (kqA.v > 0)
+    {
+        cout << "v = " << kqA.v << endl;
+        cout << "t: " << endl;
+        inDS(kqA.t);
+    }
+    else
+    {
+        cout << "Khong co ket qua" << endl;
+    }
+    cout << "-------------------------------------------------------" << endl;
+    if (kqB.p > 0 && kqB.q > 0)
+    {
+        cout << "p = " << kqB.p << endl;
+        cout << "q = " << kqB.q << endl;
+        inDS(kqB.ds);
+    }
+    else
+    {
+        cout << "Khong co phuong an" << endl;
+    }
+}
+int main()
+{
     vector<Tu> d = {
-        {"A",2,3},
-        {"B",3,3},
-        {"C",3,4},
-        {"D",5,6},
-        {"E",7,7},
-        {"G",9,8}
-    };
-    A(k,n,d);
-    cout<<"---------------------------"<<endl;
-    B(w,n,d);
+        {"A", 2, 3},
+        {"B", 3, 3},
+        {"C", 3, 4},
+        {"D", 5, 6},
+        {"E", 7, 7},
+        {"G", 9, 8}};
+    int k = 18;
+    int w = 25;
+    int n = d.size();
+    KetQuaA kqA = A(k, n, d);
+    KetQuaB kqB = B(w, n, d);
+    inKQ(kqA, kqB);
     return 0;
 }
